46130dae67459d816483bd1a4a8407b6a2c456a6
klejla <57046260+klejla@users.noreply.github.com>
2021-06-19 14:17:38 +0200

    Add files via upload

---
+++ Movies/js/main.js
@@ -0,0 +1,3 @@
+$("div.image-div").hover(function() {  // when I hover over image div
+  $(this).toggleClass("show-movie-desc");  // toggle show-movie-desc
+});
---
+++ Movies/js/smoothscroll.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){   // when the webpage loads
+  $("a").on('click', function(event) {  // we put event listener on <a> and if it gets clicked..
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+
+      // We animate scrolldown from that location to location specified by href attribute in anchor link
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
